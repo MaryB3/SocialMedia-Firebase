8ca6690bb272709ed2938dd79aa967198b6b3609
MaryB3 <mariaeugenia.dev@gmail.com>
2017-08-31 19:44:25 -0300

    Initial Commit

---
+++ SocialMediaApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SocialMediaApp
+//
+//  Created by Mary Béds on 31/08/17.
+//  Copyright © 2017 Maria Eugênia Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
